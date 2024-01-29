c0f280a026de58e678059101069832b457bddace
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-30 05:54:45 +0600

    added card

--- src/components/WhyChoose/WhyChooseEcoLife.tsx
+++ src/components/WhyChoose/WhyChooseEcoLife.tsx
@@ -19,22 +19,33 @@ const WhyChooseEcoLife = () => {
           className="hidden md:flex absolute md:-bottom-20 lg:"
         />
       </div>
+      {/* card section */}
       <div className="mt-20 px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 max-w-[1280px] mx-auto">
         {/* card */}
         {all.map(
           (data, index): pros => (
-            <div key={index} className="flex flex-col h-full justify-center items-center">
-            <div className="rounded-full mb-5 border border-[#D6F7E7FF] h-[110px] w-[110px] flex justify-center items-center overflow-hidden">
-              <img src={data.image} alt="image" className= "rounded-full object-contain h-[70px] w-[68px]" />
+            <div
+              key={index}
+              className="flex flex-col max-h-[350px]  items-center"
+            >
+              <div
+                className=" mb-5 flex justify-center items-center overflow-hidden"
+                
+              >
+                <img
+                  src={data.image}
+                  alt="image"
+                  className="rounded-full h-[110px] w-[110px] shadow-md object-cover"
+                  style={{ boxShadow: "0px 0px 10px 1px #D6F7E7" }}
+                />
+              </div>
+              <h1 className="text-[#2BAE7A] text-2xl max-w-[241px] font-semibold text-center mb-2 h-[69px]">
+                {data.heading}
+              </h1>
+              <p className="text-xl font-light max-w-[273px] text-center text-[#8F9779] max-h-[135px]">
+                {data.description}
+              </p>
             </div>
-            <h1 className="text-[#2BAE7A] text-2xl font-semibold text-center mb-2 h-16 leading-tight">
-              {data.heading}
-            </h1>
-            <p className="text-xl font-light text-center text-[#8F9779] h-16 leading-snug">
-              {data.description}
-            </p>
-          </div>
-          
           )
         )}
       </div>
--- src/dev-data/whyChoose.tsx
+++ src/dev-data/whyChoose.tsx
@@ -1,31 +1,35 @@
 type pros = {
   image: string;
-  heading: string;
+  heading: string | React.ReactNode;
   description: string;
 };
 
 const data: pros[] = [
   {
-    image: "www.google.com",
+    image: "/group1.png",
     heading: "Diverse Eco-Friendly Products",
     description:
       "Explore eco-friendly products, from sustainable fashion to green home essentials, for a variety of environmentally responsible options.",
   },
   {
-    image: "www.google.com",
+    image: "/group2.png",
     heading: "Commitment to Sustainability",
     description:
       "We prioritize sustainability, partnering with like-minded brands to reduce environmental impact and build a greener future",
   },
   {
-    image: "www.google.com",
+    image: "/group3.png",
     heading: "Expertise and Education",
     description:
       "Join our eco-conscious community for more than just shopping. Get informed with empowering articles to make sustainable choices.",
   },
   {
-    image: "www.google.com",
-    heading: "Long lasting impact",
+    image: "/group4.png",
+    heading: (
+      <>
+        Long lasting <br /> impact
+      </>
+    ),
     description:
       "You're not just shopping; Your purchases create a positive, lasting impact on the planet through our eco-initiatives.",
   },
