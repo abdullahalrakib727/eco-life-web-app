08d36ad291c40ca66b7fcbfe3f8567b0bd16ca61
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-13 12:08:40 +0600

    added featured section

--- src/components/Home/Banner/Banner.tsx
+++ src/components/Home/Banner/Banner.tsx
@@ -37,7 +37,7 @@ const Banner: FC = (): JSX.Element => {
             <br />
             products and make every purchase count
           </p>
-          <button className="uppercase mt-8 lg:mt-12 bg-[#2BAE7A] px-4 py-2 text-white lg:px-10 lg:py-4 rounded-lg hover:bg-[#D2B48C] transition duration-300 ease-linear">
+          <button className="uppercase mt-8 lg:mt-12 bg-[#2BAE7A] px-4 py-2 text-white lg:px-10 lg:py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-linear">
             Shop Now
           </button>
 
--- src/components/Home/Featured/FeaturedProducts.tsx
+++ src/components/Home/Featured/FeaturedProducts.tsx
@@ -9,16 +9,16 @@ const FeaturedProducts: FC = (): JSX.Element => {
     <section className="relative mt-16">
       <Header title="Featured Products" />
       {/* overlay */}
-      <div className="absolute left-0 -z-20 lg:translate-y-24 lg:translate-x-3 xl:translate-y-16">
+      <div className="absolute left-0 -z-20  translate-y-32 base:translate-x-8 md:translate-y-28 md:translate-x-5 lg:translate-y-24 lg:translate-x-3 xl:translate-y-16">
         <img
-          className="max-w-[150px] lg:max-w-[180px] xl:max-w-[232px] xl:max-h-[273px]"
+          className="max-w-[100px] md:max-w-[150px] lg:max-w-[180px] xl:max-w-[232px] xl:max-h-[273px]"
           src="/left.png"
           alt=""
         />
       </div>
-      <div className="absolute right-0 -z-20 lg:-translate-y-5 lg:-translate-x-3 xl:-translate-y-20">
+      <div className="absolute right-0 -z-20 translate-y-20 base:-translate-x-8  md:translate-y-5 md:-translate-x-5 lg:-translate-y-5 lg:-translate-x-3 xl:-translate-y-20">
         <img
-          className="max-w-[150px] lg:max-w-[180px] xl:max-w-[232px] xl:max-h-[273px]"
+          className="max-w-[100px] md:max-w-[150px] lg:max-w-[180px] xl:max-w-[232px] xl:max-h-[273px]"
           src="/right.png"
           alt=""
         />
