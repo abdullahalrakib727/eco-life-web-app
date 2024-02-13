b66a10c150569890f64df056489fc526cfff837a
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-13 10:06:11 +0600

    added overlay

--- src/pages/Home/Featured/FeaturedProducts.tsx
+++ src/pages/Home/Featured/FeaturedProducts.tsx
@@ -6,15 +6,23 @@ const FeaturedProducts: FC = (): JSX.Element => {
   const AllProducts = Products;
 
   return (
-    <section>
+    <section className="relative">
       <Header title="Featured Products" />
-      <div className="mt-14 grid grid-cols-1  md:grid-cols-3 gap-16 p-5">
+      {/* overlay */}
+      <div className="absolute left-0 -z-20 top-16">
+        <img className="max-w-[232px] max-h-[273px]" src="/left.png" alt="" />
+      </div>
+    <div className="absolute -z-20 right-0 -top-10">
+    <img className="max-w-[232px] max-h-[273px]" src="/right.png" alt="" />
+    </div>
+    {/* main content */}
+      <div className="mt-24 grid grid-cols-1  md:grid-cols-3 gap-16 p-16">
         {AllProducts.map((product, index) => (
-          <div className="max-w-[413px] max-h-[612px]" key={index}>
+          <div className="max-w-[413px] max-h-[612px] overflow-hidden" key={index}>
             <img
               src={product.image}
               alt="product-image"
-              className="w-[380px] lg:h-[451px] object-contain"
+              className="w-[380px] lg:h-[451px] object-contain hover:scale-105 duration-300 transition-all"
             />
             <h1 className="mt-5 text-[#36B281] text-2xl font-semibold">
               {product.name}
@@ -27,10 +35,10 @@ const FeaturedProducts: FC = (): JSX.Element => {
                 </p>
                 <p className="text-lg font-bold">{product.price} Rs.</p>
               </div>
-              <p className="text-lg font-normal">
+              <a href="#" className="text-lg font-normal border-b border-black">
                 {" "}
                 {product.reviewsNumber} reviews
-              </p>
+              </a>
             </div>
             <div className="text-center mt-5">
               <button className="inline-block text-center w-[164px] h-[42px] bg-[#2BAE7A] text-white px-7 py-2 mx-auto shadow-lg transition-all hover:scale-105 duration-300 border-b-4 border-black rounded-md hover:bg-green-700">
