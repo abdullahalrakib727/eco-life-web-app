c4e4d3706762bad9228b23d30c5a3d02e7afea2e
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-02-14 11:33:36 +0600

    added trycatch on authprovider

--- client/src/Providers/AuthProvider.tsx
+++ client/src/Providers/AuthProvider.tsx
@@ -7,17 +7,21 @@ import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
   updateProfile,
 } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/cordova";
 
 //* interfaces
 interface AuthInfo {
   loading: boolean;
+  error: string;
   createUser: (email: string, password: string) => void;
   signInUser: (email: string, password: string) => void;
   signOutUser: () => void;
   updateUser: (name: string, photo: string) => void;
+  googleSignIn: () => void;
   user: User | null;
 }
 
@@ -25,16 +29,17 @@ interface AuthProviderProps {
   children: ReactNode;
 }
 
-
 // ? default auth info
 
 const defaultAuthInfo: AuthInfo = {
   loading: false,
+  error: "",
   user: null,
   createUser: () => {},
   signInUser: () => {},
   signOutUser: () => {},
   updateUser: () => {},
+  googleSignIn: () => {},
 };
 
 // ? create context
@@ -43,40 +48,89 @@ export const AuthContext = createContext<AuthInfo>(defaultAuthInfo);
 
 const auth = getAuth(app);
 
+const provider = new GoogleAuthProvider();
+
 const AuthProvider: FC<AuthProviderProps> = ({ children }): JSX.Element => {
-  
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState<string>("");
 
   //? create user with email and password
 
-  const createUser = (email: string, password: string) => {
+  const createUser = async (email: string, password: string) => {
     setLoading(true);
-    return createUserWithEmailAndPassword(auth, email, password);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      setLoading(true);
+      if (error instanceof Error) {
+        setError(error.message);
+        console.log(error.message);
+      }
+    }
   };
 
   // ?  sign in user with email and password
 
-  const signInUser = (email: string, password: string) => {
+  const signInUser = async (email: string, password: string) => {
     setLoading(true);
-    return signInWithEmailAndPassword(auth, email, password);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setLoading(false);
+    } catch (error) {
+      setLoading(true);
+      if (error instanceof Error) {
+        setError(error.message);
+        console.log(error.message);
+      }
+    }
   };
 
   //? sign out current user
 
-  const signOutUser = () => {
+  const signOutUser = async () => {
     setLoading(true);
-    return signOut(auth);
+    try {
+      await signOut(auth);
+    } catch (error) {
+      setLoading(true);
+      if (error instanceof Error) {
+        setError(error.message);
+        console.log(error.message);
+      }
+    }
   };
 
   //? update current user
 
-  const updateUser = (name: string, photo: string) => {
-    if (auth.currentUser) {
-      return updateProfile(auth.currentUser, {
-        displayName: name,
-        photoURL: photo,
-      });
+  const updateUser = async (name: string, photo: string) => {
+    try {
+      if (auth.currentUser) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+          photoURL: photo,
+        });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        console.log(error.message);
+      }
+    }
+  };
+
+  // sign in with google
+
+  const googleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log(user);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        console.log(error.message);
+      }
     }
   };
 
@@ -105,6 +159,8 @@ const AuthProvider: FC<AuthProviderProps> = ({ children }): JSX.Element => {
     signOutUser,
     updateUser,
     user,
+    error,
+    googleSignIn,
   };
 
   return (
--- client/src/Routes/SecureRoute.tsx
+++ client/src/Routes/SecureRoute.tsx
@@ -1,27 +1,21 @@
 import { FC, ReactNode } from "react";
 import useAuth from "../hooks/useAuth";
 
-
-
 interface SecureRouteProps {
-    children:ReactNode;
-
+  children: ReactNode;
 }
 
+const SecureRoute: FC<SecureRouteProps> = ({ children }): JSX.Element => {
+  const { loading, user } = useAuth();
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (!user) {
+    return <p>Unauthorized</p>;
+  }
 
-const SecureRoute:FC<SecureRouteProps> = ({children}):JSX.Element => {
-
-    const {loading,user}  = useAuth();
-
-    if(loading){
-        return <p>Loading...</p>;
-    }
-    if(!user){
-        return <p>Unauthorized</p>;
-    }
-
-return <>{children}</>;
+  return <>{children}</>;
 };
 
 export default SecureRoute;
