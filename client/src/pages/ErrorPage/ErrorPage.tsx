91ceca83a9bb6b6b24d9c6dc35c5152c8813e51f
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-09-08 22:58:07 +0600

    Refactor ErrorPage component to use consistent indentation and add missing curly braces

--- client/src/pages/ErrorPage/ErrorPage.tsx
+++ client/src/pages/ErrorPage/ErrorPage.tsx
@@ -2,8 +2,7 @@ import { FC } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const ErrorPage: FC = (): JSX.Element => {
-
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div className=" min-h-svh flex justify-center items-center relative">
@@ -14,7 +13,12 @@ const navigate = useNavigate();
 
       <div className="absolute bottom-56 flex flex-col justify-center space-y-2">
         <h4 className=" text-[#5DBA6D] font-bold text-4xl">404 Not Found</h4>
-        <button className="border w-1/2 mx-auto bg-green-700 rounded-md text-white py-1 hover:bg-green-600" onClick={()=>{navigate('/')}}>
+        <button
+          className="border w-1/2 mx-auto bg-green-700 rounded-md text-white py-1 hover:bg-green-600"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           Back to Home
         </button>
       </div>
